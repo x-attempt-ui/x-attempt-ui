a78750fbccf8d0807ae8e27deafc31ff58ee9080
XinXiaoIsMe <ccattempt@163.com>
2024-04-08 23:01:52 +0800

    chore(husky): udpate commit message format error tip

--- .husky/scripts/verifyCommitMsg.js
+++ .husky/scripts/verifyCommitMsg.js
@@ -10,7 +10,7 @@ if (!msg.startsWith('Merge branch')) {
     = /^(v\d+\.\d+\.\d+(-(alpha|beta|rc.\d+))?)|((revert: )?(feat|fix|docs|refactor|chore|style|test|revert|ci)(\(.+\))?!?: .{1,50})/
   if (!commitRE.test(msg)) {
     console.error(
-      `${chalk.red(`\n请使用正确的格式提交 commits. 例如:\n`)
+      `${chalk.red(`\n请使用正确的格式提交 commits. \n目前可用的前缀: feat|fix|docs|refactor|chore|style|test|revert|ci\n例如:\n`)
         }${chalk.green(`feat(compiler): 增加了 commit 参数`)}\n`
         + `${chalk.green(`fix(button): 修复按钮禁用失效问题 (close #28)`)}\n`,
     )
