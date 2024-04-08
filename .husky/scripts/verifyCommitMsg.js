95042da4b862e607ec23cc31bb0ab62c64ed920d
XinXiaoIsMe <ccattempt@163.com>
2024-04-08 22:49:33 +0800

    ci(husky): add commit message format validation.

---
+++ .husky/scripts/verifyCommitMsg.js
@@ -0,0 +1,19 @@
+import fs from 'node:fs'
+import process from 'node:process'
+import chalk from 'chalk'
+
+const msgPath = '.git/COMMIT_EDITMSG'
+const msg = fs.readFileSync(msgPath, 'utf-8').trim()
+console.error('test')
+if (!msg.startsWith('Merge branch')) {
+  const commitRE
+    = /^(v\d+\.\d+\.\d+(-(alpha|beta|rc.\d+))?)|((revert: )?(feat|fix|docs|refactor|chore|style|test|revert|ci)(\(.+\))?!?: .{1,50})/
+  if (!commitRE.test(msg)) {
+    console.error(
+      `${chalk.red(`\n请使用正确的格式提交 commits. 例如:\n`)
+        }${chalk.green(`feat(compiler): 增加了 commit 参数`)}\n`
+        + `${chalk.green(`fix(button): 修复按钮禁用失效问题 (close #28)`)}\n`,
+    )
+    process.exit(1)
+  }
+}
